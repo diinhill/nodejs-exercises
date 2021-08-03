d4a7b8c0f72f3d50cdfe87b486ad2f896c11238c
diinhill <hillenbrand.nadine@hotmail.de>
2021-08-03 06:21:57 +0200

    @List.js: filter cards by search via InputField

--- src/components/List.js
+++ src/components/List.js
@@ -1,29 +1,44 @@
 import React, { useState, useEffect } from 'react'
 import ListItem from './ListItem'
+import InputField from './InputField'
+
 
 const List = () => {
 
     const [data, setData] = useState()
+    const [name, setName] = useState('')
+    const handleChange = (event) => {
+      /* console.log('event.target.value:', event.target.value) */
+      setName(event.target.value)
+    }
+
     useEffect(() => {
         const getData = async () => {
             const response = await fetch("https://rickandmortyapi.com/api/character/")
             const obj = await response.json()
-            console.log('results:', obj)
+            /* console.log('results:', obj) */
             setData(obj.results)
         }
         getData()
     }, [])
+    // console.log('data:', data)
 
+    const results = !name
+    ? data
+    : data && data.filter(person => 
+        person.name.toLowerCase().includes(name.toLocaleLowerCase()))
+        console.log("hello: ",results)
 
-    console.log('data:', data)
 
     return (
         <div>
+            <InputField name={name} handleChange={handleChange}/>
             {
-                data && 
-                data.map(item => {
+                results &&
+                results.map(item => {
+                    /* console.log('item.name:', item.name) */
                     return (
-                        <ListItem key={item.id} character={item}/>
+                            <ListItem key={item.id} character={item}/>
                     )
                 })
             }
